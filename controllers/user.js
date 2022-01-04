524c143e31a6310b3b4b3298f90161bfc9ed4214
cemcoe <chemxcode@gmail.com>
2022-01-04 21:50:36 +0800

    refactor: 抽离controller

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+
+// 获取用户列表
+const getUsersList = (ctx) => {
+  ctx.body = '获取用户列表'
+};
+
+module.exports = {
+  getUsersList,
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const Router = require('koa-router')
 const router = new Router()
+const { getUsersList } = require('../controllers/user.js')
 
 // 获取用户列表
-router.get('/users', (ctx) => {
-  ctx.body = '获取用户列表'
-})
+router.get('/users', getUsersList)
 
 module.exports = router
