77e844c4525822dcb1532561a1b268e58d4802d2
cemcoe <chemxcode@gmail.com>
2022-01-04 20:44:52 +0800

    feat: 连接数据库

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ router.get('/', (ctx, next) => {
   ctx.body = '/';
 });
 
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: '127.0.0.1',   // 数据库地址
+  user: 'root',    // 数据库用户
+  password: '123456',  // 数据库密码
+  database: 'bookbox'  // 选中数据库
+})
+
+// 执行sql脚本对数据库进行读写 
+connection.query('SELECT * FROM user', (error, results, fields) => {
+  // https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+  // This is because caching_sha2_password is introduced in MySQL 8.0, but the Node.js version is not implemented yet.
+  // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
+  if (error) throw error
+  // connected! 
+  console.log(results, 'select')
+
+});
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
