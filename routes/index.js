277da1d2c71ea7e81941435cea497b799718a534
cemcoe <chemxcode@gmail.com>
2022-01-05 09:51:03 +0800

    feat: 添加欢迎语

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const Router = require('koa-router')
 const router = new Router()
 const { getUsersList } = require('../controllers/user.js')
 
+router.get('/', (ctx) => {
+  ctx.body = '欢迎使用书盒api'
+})
+
 // 获取用户列表
 router.get('/users', getUsersList)
 
