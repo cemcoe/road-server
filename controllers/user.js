63c13a26c83e5b867047f58fb7dfcead801bfe94
cemcoe <chemxcode@gmail.com>
2022-01-06 22:22:23 +0800

    refactor: login fn mv to controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,41 @@
+const { runSqlStatement } = require('../mysql/index.js')
+const jsonwebtoken = require('jsonwebtoken')
+const { secret } = require('../config')
 
 // 获取用户列表
 const getUsersList = (ctx) => {
   ctx.body = '获取用户列表'
 };
 
+const login = async (ctx) => {
+
+  // TODO: 从请求体中取出用户名校验用户名和密码
+  const statement = `SELECT * FROM user WHERE id=1`
+  const result = await runSqlStatement(statement)
+
+  if(!result.length) {
+    // TODO: 更加友好的返回码，该用户不存在，密码错误
+    ctx.body = {
+      status: 401,
+    }
+    return 
+  }
+  const { id, name } = result[0]
+  // console.log(id, name, result)
+  const token = jsonwebtoken.sign({ id, name }, secret, { expiresIn: '1d' })
+
+  // console.log(result, 'async')
+  // 登录成功返回token
+  ctx.body = {
+    status: 200,
+    data: {
+      token,
+    }
+  }
+}
+
 module.exports = {
   getUsersList,
+  login,
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const Router = require('koa-router')
 const router = new Router()
-
-const jsonwebtoken = require('jsonwebtoken')
 const jwt = require('koa-jwt')
 
 const { secret } = require('../config')
-const { getUsersList } = require('../controllers/user.js')
-const { runSqlStatement } = require('../mysql/index.js')
+const { getUsersList, login } = require('../controllers/user.js')
 
 router.get('/', (ctx) => {
   ctx.body = '欢迎使用书盒api'
@@ -16,32 +13,7 @@ router.get('/', (ctx) => {
 router.get('/v1/users', getUsersList)
 
 // 用户登录
-router.get('/v1/login', async (ctx) => {
-
-  // TODO: 从请求体中取出用户名校验用户名和密码
-  const statement = `SELECT * FROM user WHERE id=10`
-  const result = await runSqlStatement(statement)
-
-  if(!result.length) {
-    // TODO: 更加友好的返回码，该用户不存在，密码错误
-    ctx.body = {
-      status: 401,
-    }
-    return 
-  }
-  const { id, name } = result[0]
-  // console.log(id, name, result)
-  const token = jsonwebtoken.sign({ id, name }, secret, { expiresIn: '1d' })
-
-  // console.log(result, 'async')
-  // 登录成功返回token
-  ctx.body = {
-    status: 200,
-    data: {
-      token,
-    }
-  }
-})
+router.get('/v1/login', login)
 
 const auth = jwt({ secret })
 // 获取登录用户信息
