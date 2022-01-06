15b54ad92ec2ecd0c543bd66c8dfa6f26380e0f3
cemcoe <chemxcode@gmail.com>
2022-01-06 12:54:05 +0800

    feat: api add version

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const { getUsersList } = require('../controllers/user.js')
 router.get('/', (ctx) => {
   ctx.body = '欢迎使用书盒api'
 })
-
+ 
 // 获取用户列表
-router.get('/users', getUsersList)
+router.get('/v1/users', getUsersList)
 
 // 用户登录
-router.get('/login', (ctx) => {
+router.get('/v1/login', (ctx) => {
   ctx.body = '用户登录'
 })
 
