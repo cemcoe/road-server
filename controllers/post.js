b4306d09a34f070a62e8533da76fe41137e3f7a2
cemcoe <chemxcode@gmail.com>
2022-01-12 11:16:10 +0800

    feat: createPost add title abstract struct

--- controllers/post.js
+++ controllers/post.js
@@ -1,24 +1,33 @@
 const { runSqlStatement } = require('../mysql/index.js')
 
 const createPost = async (ctx) => {
-  console.log(ctx.request.body)
+  // console.log(ctx.request.body)
   // TODO: 从请求体中取出用户名校验用户名和密码
   // 借助 koa-body 解析body参数
 
   // 拿到登录用户的id
   const { id } = ctx.state.user
-  const { content } = ctx.request.body
+  // const { title = '默认标题', abstract, content } = ctx.request.body
+  // 上面的语法有待商榷
+  // 当要提取的对象对应属性解析为 undefined，变量就被赋予默认值。前端可能传过来空字符串
   // TODO: 生成摘要等，丰富文章信息
+  const { content } = ctx.request.body
+  const title = ctx.request.body.title || '默认标题'
+  const abstract = ctx.request.body.abstract || content.slice(0, 20)
 
-  const statement = `INSERT INTO post(authorId, content) VALUES('${id}', '${content}');`
+  // 根据内容生成摘要
+  // const abstract = content.slice(0, 20)
+
+  const statement = `INSERT INTO post(title, authorId, content, abstract) VALUES('${title}', '${id}', '${content}', '${abstract}');`
   const result = await runSqlStatement(statement)
-  // console.log(result)
+  // console.log('--', result.insertId, '--')
 
   if (result) {
+    const { insertId } = result
     ctx.body = {
       status: 200,
       data: {
-        msg: '成功',
+        insertId,
       }
     }
   }
