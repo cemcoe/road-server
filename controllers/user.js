c79ad5eb09cf4f273ecd66b2a82dd9bfd2e6d1fb
cemcoe <chemxcode@gmail.com>
2022-11-18 10:59:04 +0800

    creat

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,56 @@
-const { runSqlStatement } = require('../mysql/index.js')
-const jsonwebtoken = require('jsonwebtoken')
-const { secret } = require('../config')
+const { runSqlStatement } = require("../mysql/index.js");
+const jsonwebtoken = require("jsonwebtoken");
+const { secret } = require("../config");
+
+// 用户注册
+const create = (ctx) => {
+  const { name, password } = ctx.request.body;
+  console.log("create");
+
+  ctx.body = {
+    status: 200,
+    user: {
+      name,
+      password,
+    },
+  };
+};
 
 // 获取用户列表
 const getUsersList = async (ctx) => {
-
   // 查询用户列表
-  const statement = `SELECT * FROM user`
-  const result = await runSqlStatement(statement)
-  console.log(result)
+  const statement = `SELECT * FROM user`;
+  const result = await runSqlStatement(statement);
+  console.log(result);
 
   ctx.body = {
     status: 200,
     data: {
-      users: result
-    }
-
-  }
+      users: result,
+    },
+  };
 };
 
 const login = async (ctx) => {
-  console.log(ctx.request.body)
+  console.log(ctx.request.body);
   // TODO: 从请求体中取出用户名校验用户名和密码
   // 借助 koa-body 解析body参数
-  const { name, password } = ctx.request.body
-  const statement = `SELECT * FROM user WHERE name='${name}' AND password='${password}'`
-  const result = await runSqlStatement(statement)
-  console.log(result)
+  const { name, password } = ctx.request.body;
+  const statement = `SELECT * FROM user WHERE name='${name}' AND password='${password}'`;
+  const result = await runSqlStatement(statement);
+  console.log(result);
 
   if (!result.length) {
     // TODO: 更加友好的返回码，该用户不存在，密码错误
     // 没有匹配到对应的用户名和密码
     ctx.body = {
       status: 401,
-    }
-    return
+    };
+    return;
   }
-  const { id } = result[0]
+  const { id } = result[0];
   // console.log(id, name, result)
-  const token = jsonwebtoken.sign({ id, name }, secret, { expiresIn: '1d' })
+  const token = jsonwebtoken.sign({ id, name }, secret, { expiresIn: "1d" });
   // console.log(result, 'async')
   // 登录成功返回token
   ctx.body = {
@@ -46,93 +58,90 @@ const login = async (ctx) => {
     status: 200,
     data: {
       token,
-    }
-  }
-}
+    },
+  };
+};
 
 const getOwnerInfo = async (ctx) => {
   // 拿到登录用户的id
   // console.log(ctx.state, 'state')
 
-  const { id } = ctx.state.user
+  const { id } = ctx.state.user;
   // 查数据库找用户详细信息
 
-  const statement = `SELECT * FROM user WHERE id='${id}'`
-  const result = await runSqlStatement(statement)
+  const statement = `SELECT * FROM user WHERE id='${id}'`;
+  const result = await runSqlStatement(statement);
 
   if (!result.length) {
     // TODO: 更加友好的返回码，该用户不存在，密码错误
     // 没有匹配到对应的用户名和密码
     ctx.body = {
       status: 401,
-    }
-    return
+    };
+    return;
   }
 
   ctx.body = {
     status: 200,
     data: {
-      ...result[0]
-    }
-  }
-}
-
+      ...result[0],
+    },
+  };
+};
 
 const getUserDetail = async (ctx) => {
-  const { uid } = ctx.params
+  const { uid } = ctx.params;
   const statement = `
   SELECT id, name, avatar, gender
   FROM user
-  WHERE id = ${uid};`
+  WHERE id = ${uid};`;
 
-  const result = await runSqlStatement(statement)
+  const result = await runSqlStatement(statement);
 
   if (!result.length) {
     ctx.body = {
       status: 404,
-    }
-    return
+    };
+    return;
   }
   // console.log(result)
   ctx.body = {
     status: 200,
     data: {
-      user: result[0]
-    }
-  }
-}
+      user: result[0],
+    },
+  };
+};
 
-const getUserPostList = async(ctx) => {
-  const { uid } = ctx.params
+const getUserPostList = async (ctx) => {
+  const { uid } = ctx.params;
   const statement = `
   SELECT p.id, p.title, p.abstract
   FROM post p
-  WHERE p.authorId = ${uid};`
+  WHERE p.authorId = ${uid};`;
 
-  const result = await runSqlStatement(statement)
+  const result = await runSqlStatement(statement);
 
   if (!result.length) {
     ctx.body = {
       status: 404,
-    }
-    return
+    };
+    return;
   }
   // console.log(result)
   ctx.body = {
     status: 200,
     data: {
-      postList: result
-    }
-  }
-
-}
-
+      postList: result,
+    },
+  };
+};
 
 module.exports = {
+  create,
   getUsersList,
   login,
   getOwnerInfo,
   getUserDetail,
   getUserPostList,
-}
-
+};
--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,58 @@
-const Router = require('koa-router')
-const router = new Router()
-const jwt = require('koa-jwt')
+const Router = require("koa-router");
+const router = new Router();
+const jwt = require("koa-jwt");
 
-const { secret } = require('../config')
+const { secret } = require("../config");
 const {
+  create,
   getUsersList,
   login,
   getOwnerInfo,
   getUserDetail,
   getUserPostList,
-} = require('../controllers/user.js')
+} = require("../controllers/user.js");
 
-const { createPost, getPostList, getPostDetail, updatePost } = require('../controllers/post.js')
+const {
+  createPost,
+  getPostList,
+  getPostDetail,
+  updatePost,
+} = require("../controllers/post.js");
 
-router.get('/', (ctx) => {
-  ctx.body = '欢迎使用书盒api'
-})
+router.get("/", (ctx) => {
+  ctx.body = "欢迎使用书盒api";
+});
+
+// 用户注册
+router.post("/v1/user", create);
 
 // 获取用户列表
-router.get('/v1/users', getUsersList)
+router.get("/v1/users", getUsersList);
 
 // 用户登录
-router.post('/v1/login', login)
+router.post("/v1/login", login);
 
-const auth = jwt({ secret })
+const auth = jwt({ secret });
 // 获取登录用户信息
-router.get('/v1/owner', auth, getOwnerInfo)
+router.get("/v1/owner", auth, getOwnerInfo);
 
 // ------文章相关------
 // 创建新文章
-router.post('/v1/post', auth, createPost)
+router.post("/v1/post", auth, createPost);
 
 // 更新文章
-router.put('/v1/post/:pid', updatePost)
+router.put("/v1/post/:pid", updatePost);
 
 // 获取文章列表
-router.get('/v1/posts', getPostList)
+router.get("/v1/posts", getPostList);
 
 // 获取文章详情
-router.get('/v1/post/:id', getPostDetail)
-
+router.get("/v1/post/:id", getPostDetail);
 
 // 获取用户详情
-router.get('/v1/user/:uid', getUserDetail)
+router.get("/v1/user/:uid", getUserDetail);
 
 // 获取用户文章列表
-router.get('/v1/user/:uid/posts', getUserPostList)
+router.get("/v1/user/:uid/posts", getUserPostList);
 
-
-
-
-module.exports = router
+module.exports = router;
