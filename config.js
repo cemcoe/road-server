9e65539aaca62860df60dad180d041b4e1986321
cemcoe <chemxcode@gmail.com>
2023-01-31 09:24:29 +0800

    go

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const PORT = 3001; // 服务监听端口
 const sqlConnectionStr = {
   host: "127.0.0.1", // 数据库地址
   user: "root", // 数据库用户
-  password: "123456", // 数据库密码
+  password: "cemcoe@2025", // 数据库密码
   database: "road", // 选中数据库
 };
 
